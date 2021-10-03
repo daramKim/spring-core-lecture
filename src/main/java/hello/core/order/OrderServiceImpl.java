d0935bdd23322666cc001ec114be1c6cdf01f5a4
daramKim <cjw0672@gmail.com>
2021-10-04 03:30:05 +0900

    고정할인 정책 및 주문서비스 추가

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.memoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    MemberRepository memberRepository = new memoryMemberRepository();
+    DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member findMember = memberRepository.findById(memberId);
+        int discountAmt = discountPolicy.discount(findMember, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountAmt);
+    }
+}
