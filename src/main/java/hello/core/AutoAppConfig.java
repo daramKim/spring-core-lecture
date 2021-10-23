d47dd0ab77651ec7bbb2c403a43cd02b2813deaa
daramKim <cjw0672@gmail.com>
2021-10-23 21:36:57 +0900

    스프링 Bean 중복 등록시, 충돌 매커니즘

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -71,6 +74,32 @@ import org.springframework.context.annotation.FilterType;
 * 💡 참고: 사실 애노테이션에는 상속관계라는 것이 없다. 그래서 애노테이션이 특정 애노테이션을 포함하고 있는 것을 인식할 수 있는 것은
 *         자바 언어가 지원하는 기능이 아니고, "스프링"에서 지원하는 기능이다.
 *
+* */
+
+// 중복 등록과 충돌
+/*
+* 컴포넌트 스캔에서 같은 빈 이름을 등록하면 어떻게 될까?
+* 다음 두가지 상황이 있다.
+*
+* 1. 자동 빈 등록 vs 자동 빈 등록
+*  - 컴포넌트 스캔에 의해 자동으로 스프링 빈이 등록되는데, 그 이름이 같은 경우 스프링은 오류를 발생시킨다.
+*    (ConfilictingBeanDefinitionException)
+* 2. 수동 빈 등록 vs 자동 빈 등록
+*  - 이 경우 수동 빈 등록이 우선권을 가진다.(수동 빈이 자동 빈을 오버라이딩 해버린다.)
+*  - 수동 빈 등록시 남는 로그
+*    Overriding bean definition for bean 'memoryMemberRepository' with a different
+     definition: replacing
+
+*  - 물론 개발자가 의도적으로 이런 결과를 기대했다면, 자동 보다는 수동이 우선권을 가지는 것이 좋다
+*    하지만 현실은 개발자가 의도적으로 설정해서 이런 결과가 만들어지기 보다는 여러 설정들이 꼬여서 이런 결과가 만들어지는 경우가 대부분이다!
+*    그러면 정말 잡기 어려운 버그가 만들어진다. 항상 잡기 어려운 버그는 애매한 버그다
+*  - 그래서 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+*  - 수동 빈 등록, 자동 빈 등록 오류시 스프링 부트 에러
+*    Consider renaming one of the beans or enabling overriding by setting
+     spring.main.allow-bean-definition-overriding=true
+
+*  - 만약 수동 빈 등록(오버라이딩)이 가능하게 하려면 application.properties에 값을 추가하면 된다.
+*    spring.main.allow-bean-definition-overriding=true
 *
 * */
 
@@ -80,4 +109,9 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    // 테스트용으로 이미 @Component 어노테이션이 등록된 Bean을 추가적으로 수동 등록 빈 구성으로 추가..
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
