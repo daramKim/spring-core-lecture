37f80c28855cf779dce6597b5aaa40aafe66da6b
daramKim <cjw0672@gmail.com>
2021-10-23 21:57:33 +0900

    의존관계 주입1  - 생성자 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,18 +3,23 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    MemberRepository memberRepository;
-    DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     // @Autowired 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다.
-    @Autowired
+    // 생성자가 딱 1개만 있으면 @Autowired 를 생략해도 자동주입이 된다. (물론 스프링 빈일 경우만)
+    // 특징
+    // 1. 생성자 호출시점에 딱 1번만 호출되는 것이 보장된다.
+    // 2. 불편, 필수 의존관계에 사용
+    // @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -28,9 +33,5 @@ public class OrderServiceImpl implements OrderService {
 
     public MemberRepository getMemberRepository() {
         return memberRepository;
-    }
-
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
     }
 }
