95dfbc4e28c830f5fb1a03df5f8b4aff1dc5f14e
daramKim <cjw0672@gmail.com>
2021-10-25 20:34:15 +0900

    스프링 빈 생명주기 콜백 [ 최종 정리 ] - @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -51,7 +51,8 @@ public class BeanLifeCycleTest {
         * 따라서 직접 스프링 빈으로 등록하면 종료 메서드는 따로 적어주지 않아도 잘 동작한다.
         * 추론 기능을 사용하기 싫으면 destroyMethod="" 처럼 빈 공백을 지정하면 된다.
         * */
-        @Bean(initMethod = "init", destroyMethod = "close")
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient(){ // 인터페이스 없이 바로 구현체만 정의했음.
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,21 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+/*
+* javax 로 시작하는 패키지명은 자바 진영에서 공식적으로 지원한다는 의미임. 즉, 스프링이 아니더라도 작동한다는 의미임.
+* [ @PostConstruct, @PreDestroy 애노테이션 특징 ]
+*  - 최신 스프링에서 가장 권장하는 방법이다.
+*  - 애노테이션 하나만 붙이면 되므로 매우 편리하다.
+*  - 패키지를 잘 보면 javax.annotation.PostConstruct 이다. 스프링에 종속적인 기술이 아니라 "JSR-250" 라는 자바 표준이다. 따라서 스프링이 아닌 다른 컨테이너에서도 동작한다.
+*  - 컴포넌트 스캔과 잘 어울린다. (메서드 지정방식은 @bean에 써야하지만 그럴 필요가 없음.)
+*  - 유일한 단점은 외부 라이브러리에는 적용하지 못한다는 것이다. 외부 라이브러리를 초기화, 종료해야 하면 @Bean의 기능을 사용하자.
+*
+* [ 정리 ]
+* @PostConstruct, @PreDestroy 애노테이션을 사용하자.
+* 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면 @Bean 의 initMethod , destroyMethod 를 사용하자.
+*
+* */
 public class NetworkClient {
 
     private String url;
@@ -26,12 +42,14 @@ public class NetworkClient {
         System.out.println("close: "+ url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
